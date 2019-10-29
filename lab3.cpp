710adf55a7d60b2ee433ba61bc011dee356d026c
Dan <daniyar.burakaev@mail.ru>
2019-10-29 22:34:32 +0300

    Finally! F*ckin' elusive char arguments!

--- lab3.cpp
+++ lab3.cpp
@@ -1,3 +1,6 @@
+// add killing lagging process
+// check memory division
+
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
@@ -21,55 +24,45 @@ void custom_sleep(int sec) {
 */
 
 int main(int argv, char *argc[]) {
-    int pid1, pid2, status;
-    char filename[FILENAME_MAX];
+    int pid1, pid2;
+    int status;
+    char filename[50] = "LOG";
     int del_sec[] = {atoi(argc[1]), atoi(argc[2]), atoi(argc[3])};
     
-    cout << "Input log file name: ";
-	cin >> filename;
-    FILE *fn = fopen(filename, "w");
+    // cout << "Input log file name: ";
+	// cin >> filename;
+    FILE *fn = fopen("LOG", "w");
     fclose(fn);
     
     pid1 = fork();
-    cout << endl << "FORK ENTER" << endl;
     if(pid1 > 0) {
-		cout << endl << "VFORK ENTER" << endl;
 		pid2 = vfork();
+		if(pid2 == 0) {
+			execl("/home/dan/Desktop/sub_prog", "Descendant #2", "LOG", argc[3], (char*) 0);
+		}
 	}
 
     if(pid1 < 0 || pid2 < 0) {
 		cout << "ERROR" << endl;
 		return 0;
 	}
-    else if(pid1 == 0 && pid2 != 0) {
-		cout << endl << "PID1 HERE!" << pid2 << endl;
+    else if(pid1 == 0) {
 		sleep(del_sec[1]);
-		cout << endl << "PID1 IS READY!" << pid2 << endl;
 		fn = fopen(filename, "a");
 		proc_attr_out(getpid(), fn, (char*) "Descendant #1");
 		proc_attr_outt(getpid(), (char*) "Descendant #1");
 		fclose(fn);
 		exit(EXIT_SUCCESS);
 	}
-	else if(pid2 == 0) {
-		cout << endl << "PID2 HERE!" << pid1 << endl;
-		sleep(del_sec[2]);
-		cout << endl << "PID2 IS READY!" << pid2 << endl;
-		fn = fopen(filename, "a");
-		execl("/home/dan/Desktop/sub_prog", "Descendant #2", filename, (char*) 0);
-		fclose(fn);
-		exit(EXIT_SUCCESS);
-	}
 	else if(pid1 > 0 && pid2 > 0) {
-		cout << endl << "PAPA IS HERE!" << pid1 << pid2 << endl;
 		sleep(del_sec[0]);
-		cout << endl << "PAPA IS READY!" << pid1 << pid2 << endl;
 		fn = fopen(filename, "a");
 		proc_attr_out(getpid(), fn, (char*) "Ancesor (main process)");
 		proc_attr_outt(getpid(), (char*) "Ancesor (main process)");
 		fclose(fn);
 	}
 	
-	// waitpid(pid1, &status, 0);
+	waitpid(pid1, &status, 0);
+	// fclose(fn);
 	return 0;
 }
--- sub_prog.cpp
+++ sub_prog.cpp
@@ -1,19 +1,23 @@
 #include <sys/types.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <iostream>
 
 #include </home/dan/Desktop/sub_prog.h>
 using namespace std;
 
 int main(int argv, char *argc[]) {
-	// argc == {"Descendant #2", filename}
+
+	// argc == {"Descendant #2", filename, delay}
+	sleep(atoi(argc[2]));
+	
 	FILE *fn = fopen(argc[1], "a");
 	
-	proc_attr_out(getpid(), fn, argc[0]);
 	proc_attr_outt(getpid(), argc[0]);
+	proc_attr_out(getpid(), fn, argc[0]);
 	
 	fclose(fn);
-	return 0;
+	exit(EXIT_SUCCESS);
 }
 
 
